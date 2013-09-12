3bafa8e302b9d017bf5e63d09baac710e366250b
PublicFess <publicfess@gmail.com>
2013-09-12 11:44:13 +0400

    Initial import.

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express')
+  , http = require('http')
+  , stylus = require('stylus')
+  , nib = require('nib');
+
+var port = process.env.PORT || 3000;
+var publicPath = __dirname + '/public';
+var app = module.exports = exports = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(stylus.middleware({
+  src: publicPath,
+  compile: function(str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .set('compress', true)
+      .use(nib());
+  }
+}));
+app.use(express.static(publicPath));
+
+if (app.get('env') == 'development') {
+  app.use(express.errorHandler());
+}
+
+require('./routes');
+
+http.createServer(app).listen(port, function() {
+  console.log('Server is listening on ' + port);
+});
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = require('../app');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
